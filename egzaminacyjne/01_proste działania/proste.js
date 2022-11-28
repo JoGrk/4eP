020a495b67330ed1a995b14a2d66b6b9f3917db8
Jola Gryko <jgryko@gmail.com>
2022-11-28 13:21:59 +0100

    kwadrat

--- egzaminacyjne/01_proste działania/proste.js
+++ egzaminacyjne/01_proste działania/proste.js
@@ -1,13 +1,35 @@
 function calculate(sign){
-    const first = document.getElementById('firstNumber').value;
-    const second = document.getElementById('secondNumber').value;
+    let first = document.getElementById('firstNumber').value;
+    let second = document.getElementById('secondNumber').value;
     const result = document.getElementById('result')
 
     if(first.length==0 || second.length==0){
         result.innerHTML=("Proszę uzupełnić  obie liczby")
     }
     else{
-        console.log("Jest dobrze")
+        first = parseInt(first)
+        second = parseInt(second)
+        
+        switch(sign){
+            case '+':
+                result.innerHTML=` ${first}+ ${second}=${first+second}`
+                break;
+            case '-':
+                result.innerHTML = `${first} - ${second} = ${first-second}`
+                break;
+            case '*':
+                result.innerHTML = `${first} * ${second} = ${first*second}`
+                break;
+            case '/':
+                if(second == 0){
+                    result.innerHTML='nie wolno dzielic przez zero'
+                }else{
+                    result.innerHTML = `${first} / ${second} = ${first/second}`
+                }
+                break;
+        }
+        
+
     }
 
     
