b04056984a3dcf3f6d954e36daf179e184f3bc68
Jola Gryko <jgryko@gmail.com>
2022-12-01 15:40:06 +0100

    kk

---
+++ egzaminacyjne/06_kafelkowanie/kafelkowanie.js
@@ -0,0 +1,16 @@
+function calculatePrice(){
+    const area = document.getElementById('area');
+    const square = document.getElementById('square');
+    const rectangle = document.getElementById('rectangle');
+    const result = document.getElementById('result');
+    let price=0;
+
+    if(square.checked){
+        price=70*area.value
+    }
+    else{
+        price=80*area.value;
+    }
+
+    result.innerHTML = `Koszt kafelkowania ${price} zł `
+}
