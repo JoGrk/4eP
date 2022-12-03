1b2ddd9980cfb2a5204cb46d1bdf0006d4813c95
Jolanta Gryko <jgryko@gmail.com>
2022-12-03 08:23:42 +0100

    1

--- egzaminacyjne/03_zmiana_jednostek.html/zmiana_jednostek.js
+++ egzaminacyjne/03_zmiana_jednostek.html/zmiana_jednostek.js
@@ -1,21 +1,20 @@
-function kelwin(){
-    const celsius = document.getElementById('celsius');
-    const resultDiv = document.querySelector('#result')
-    if(celsius.value==='' || isNaN(celsius.value)){
-        alert('Błąd danych');
-    }else{
-        const degrees = parseInt(celsius.value)
-        resultDiv.textContent=degrees+273.15
-    }
+function kelwin() {
+  const celsius = document.getElementById("celsius");
+  const resultDiv = document.querySelector("#result");
+  if (celsius.value === "" || isNaN(celsius.value)) {
+    alert("Błąd danych");
+  } else {
+    const degrees = parseInt(celsius.value);
+    resultDiv.textContent = degrees + 273.15;
+  }
 }
-function fahren(){
-    const celsius= document.getElementById('celsius');
-    const result = document.getElementById('result')
-    if (celsius.value==='' || isNaN(celsius.value)) {
-        alert ('Błąd danych'); 
-    }
-    else {
-        const innaczej = parseInt(celsius.value)
-        result.textContent = (innaczej*1.8) + 32
-    }
+function fahren() {
+  const celsius = document.getElementById("celsius");
+  const result = document.getElementById("result");
+  if (celsius.value === "" || isNaN(celsius.value)) {
+    alert("Błąd danych");
+  } else {
+    const innaczej = parseInt(celsius.value);
+    result.textContent = innaczej * 1.8 + 32;
+  }
 }
--- egzaminacyjne/06_kafelkowanie/kafelkowanie.js
+++ egzaminacyjne/06_kafelkowanie/kafelkowanie.js
@@ -1,16 +1,15 @@
-function calculatePrice(){
-    const area = document.getElementById('area');
-    const square = document.getElementById('square');
-    const rectangle = document.getElementById('rectangle');
-    const result = document.getElementById('result');
-    let price=0;
+function calculatePrice() {
+  const area = document.getElementById("area");
+  const square = document.getElementById("square");
+  const rectangle = document.getElementById("rectangle");
+  const result = document.getElementById("result");
+  let price = 0;
 
-    if(square.checked){
-        price=70*area.value
-    }
-    else{
-        price=80*area.value;
-    }
+  if (square.checked) {
+    price = 70 * area.value;
+  } else {
+    price = 80 * area.value;
+  }
 
-    result.innerHTML = `Koszt kafelkowania ${price} zł `
+  result.innerHTML = `Koszt kafelkowania ${price} zł `;
 }
