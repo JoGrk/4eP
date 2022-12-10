7289d584b556ef14d31bf79ec1949e3fdbe65712
Jolanta Gryko <jgryko@gmail.com>
2022-12-10 10:52:06 +0100

    porzadki

--- egzaminacyjne/11_wesele/wesele.js
+++ egzaminacyjne/11_wesele/wesele.js
@@ -1,20 +1,17 @@
-const guests = document.querySelector('#guests');
-const improve = document.querySelector('#improve');
-const count = document.querySelector('#count');
-const result = document.querySelector('#result');
+const guests = document.querySelector("#guests");
+const improve = document.querySelector("#improve");
+const count = document.querySelector("#count");
+const result = document.querySelector("#result");
 
-count.addEventListener('click', ()=>{
-    let guestsN = parseInt(guests.value)
-    if (isNaN(guestsN) ||  guestsN<=0){
-        result.innerHTML = 'Podaj liczbę gości'
-
+count.addEventListener("click", () => {
+  let guestsN = parseInt(guests.value);
+  if (isNaN(guestsN) || guestsN <= 0) {
+    result.innerHTML = "Podaj liczbę gości";
+  } else {
+    let price = guestsN * 100;
+    if (improve.checked) {
+      price = price * 1.3;
     }
-    else{
-        let price = guestsN * 100
-        if(improve.checked){
-            price = price * 1.30
-        }
-        result.innerHTML=`Koszt twojego wesela to ${price}zł`
-    }
-    
-})
+    result.innerHTML = `Koszt twojego wesela to ${price}zł`;
+  }
+});
